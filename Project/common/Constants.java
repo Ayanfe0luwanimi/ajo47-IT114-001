05604fd89e9ae7427629643f3072118afdc50083
Ayanfe0luwanimi <Olunugaayanfe@gmail.com>
2023-12-10 02:31:22 +0300

    Milestone 2 submission

---
+++ Project/common/Constants.java
@@ -0,0 +1,10 @@
+package Project.common;
+
+
+
+public abstract class Constants {
+    public static final long DEFAULT_CLIENT_ID = -1L;
+    public static final int MINIMUM_PLAYERS = 2;
+    public static final String LOBBY = "lobby";
+    public static final int max_attempts=1;
+}
